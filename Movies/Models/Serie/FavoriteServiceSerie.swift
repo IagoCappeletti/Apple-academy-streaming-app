d405011e5153d18dfec6daff6c66a6cb8677fda4
iago cappeletti <iago_cappeletti@hotmail.com>
2024-04-30 21:58:50 -0300

    feat: criando estrutura da serie

--- Movies/Models/Serie/FavoriteServiceSerie.swift
+++ Movies/Models/Serie/FavoriteServiceSerie.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+class FavoriteServiceSerie {
+    
+    // Singleton instance
+    static let shared = FavoriteServiceSerie()
+    private init() {}
+    
+    // In memory data
+    private var favoriteSerie: [Serie] = []
+    
+    func listAll() -> [Serie] {
+        favoriteSerie
+    }
+    
+    func isFavorite(serieId: String) -> Bool {
+        favoriteSerie.contains { serie in
+            serie.id == serieId
+        }
+    }
+    
+    func addSerie(_ serie: Serie) {
+        favoriteSerie.append(serie)
+    }
+    
+    func removeSerie(withId serieId: String) {
+        favoriteSerie.removeAll { serie in
+            serie.id == serieId
+        }
+    }
+}
