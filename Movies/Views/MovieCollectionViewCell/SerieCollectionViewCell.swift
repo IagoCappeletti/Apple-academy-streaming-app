fbd9d8e018fd7c55224628642333ede2a6aa5976
ios-noite-6 <ios-noite-6@MacBook-Pro-de-Administrador.local>
2024-07-05 14:04:01 -0300

    create class SerieCollectionViewCell

--- Movies/Views/MovieCollectionViewCell/SerieCollectionViewCell.swift
+++ Movies/Views/MovieCollectionViewCell/SerieCollectionViewCell.swift
@@ -9,4 +9,49 @@ import UIKit
 
 class SerieCollectionViewCell: UICollectionViewCell {
     
+    static let identifier = "serieCollectionCell"
+    
+    // Outlets
+    @IBOutlet weak var serieImageView: UIImageView!
+    @IBOutlet weak var serieTitleLabel: UILabel!
+    
+    private let serieService = SerieService()
+    
+    func setup(serie: Serie) {
+        serieImageView.layer.masksToBounds = true
+        serieImageView.layer.cornerRadius = 8
+        serieImageView.layer.borderWidth = 1
+        serieImageView.layer.borderColor = UIColor.black.cgColor
+        
+        // Clean data
+        serieImageView.image = nil
+        serieTitleLabel.text = nil
+        
+        // Load movie poster from URL
+        if let posterURL = serie.posterURL {
+            serieService.loadImageData(fromURL: posterURL) { imageData in
+                self.updateCell(withImageData: imageData, orTitle: serie.title)
+            }
+        }
+    }
+    
+    private func updateCell(withImageData imageData: Data?, orTitle title: String) {
+        DispatchQueue.main.async {
+            if let imageData = imageData {
+                // Show movie image
+                let serieImage = UIImage(data: imageData)
+                self.serieImageView.image = serieImage
+            } else {
+                // Show movie title if poster is not available
+                self.serieTitleLabel.text = title
+            }
+        }
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        serieImageView.image = nil
+        serieTitleLabel.text = nil
+    }
+    
 }
