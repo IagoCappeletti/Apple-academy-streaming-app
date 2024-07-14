42422cd761b515d0652ce82f70de4953b5dc0c12
ios-noite-6 <ios-noite-6@MacBook-Pro-de-Administrador.local>
2024-07-14 20:34:32 -0300

    criando Serie PopUp Notification View

---
+++ Movies/Views/MoviePopUp/SeriePopUpNotificatiion.swift
@@ -0,0 +1,20 @@
+//
+//  SeriePopUpNotificatiion.swift
+//  Movies
+//
+//  Created by ios-noite-6 on 14/07/24.
+//
+
+import UIKit
+
+class SeriePopUpNotificatiion: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
