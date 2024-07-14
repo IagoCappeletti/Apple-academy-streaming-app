a42b8e8999312e9c9fc42edc9552a59804d8ee53
ios-noite-6 <ios-noite-6@MacBook-Pro-de-Administrador.local>
2024-07-14 20:35:25 -0300

    criando classe SeriePopUpNotificatiion

--- Movies/Views/MoviePopUp/SeriePopUpNotificatiion.swift
+++ Movies/Views/MoviePopUp/SeriePopUpNotificatiion.swift
@@ -9,12 +9,30 @@ import UIKit
 
 class SeriePopUpNotificatiion: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBOutlet weak var buttonClose: UIButton!
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
     }
-    */
 
+    override init(frame: CGRect){
+        super.init(frame: frame)
+
+        setupXib(frame: CGRect(x: 0, y: 0, width: frame.width, height: frame.height ))
+    }
+    
+    func setupXib(frame: CGRect){
+        let view = loadXib()
+        view.frame = frame
+        addSubview(view)
+    }
+    
+    func loadXib() -> UIView{
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: "SeriePopUpNotificationView", bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil).first as! UIView
+        
+        return view
+    }
+    
 }
