432ebcbf0a567d54720f1d780f7fab00a2ba1c72
ios-noite-6 <ios-noite-6@10-41-49-140.0200.epoa.br.iosda.org>
2024-07-25 21:25:19 -0300

    fix/serieService

--- Movies/Models/SerieService.swift
+++ Movies/Models/SerieService.swift
@@ -31,6 +31,7 @@ struct SerieService {
         let request = URLRequest(url: url)
         
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            
             if data == nil &&
                 response == nil {
                 completion(nil)
